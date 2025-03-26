781b47838cdfc3816c84553f8c66853b97e38779
Aleksei Nechaev <omfg.rus@gmail.com>
2025-03-26 08:19:57 +0700

    Separation of classes

--- php_numpower.h
+++ php_numpower.h
@@ -62,6 +62,7 @@ ZEND_FUNCTION(print_r_);
 
 
 PHPAPI zend_class_entry *phpsci_ce_NDArray;
+PHPAPI zend_class_entry *phpsci_ce_NumPower;
 PHPAPI zend_class_entry *phpsci_ce_ArithmeticOperand;
 
 # define PHP_NDARRAY_VERSION "0.7.0"
